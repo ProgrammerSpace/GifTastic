bbe17192b0be99847638b9b8714eab72cb6ab9e2
MeenaKrs <46361811+MeenaKrs@users.noreply.github.com>
2019-04-19 22:38:17 -0700

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,10 @@
+// Global variables
 var reactions = ["excited", "facepalm", "omg", "confused", "amazing"];
 var favorites = [];
+var ifAddMore = "", addPresent = false;
+var offset = 0, max = 0;
 
+// Create Buttons
 function createButtons() {
 
     for (let i = 0; i < reactions.length; i++) {
@@ -13,21 +17,32 @@ function createButtons() {
 
 }
 
-function displayGifs() {
+// Get gifs from giphy, Make blocks with related info for every gif, Display elements in UI
+function displayGifs(arg) {
 
-    $("#gifs-appear-here").empty();
+    if (arg == "add") {
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + ifAddMore + "&api_key=2GuZlr3V4NmdWt8DPnAjUoO38tsH5BPs";
+        if (offset == 0) {
+            offset += 10;
+        } else {
+            offset += 5;
+        }
+        max = offset + 5;
+    } else {
+        $("#gifs-appear-here").empty();
 
-    var reaction = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + reaction + "&api_key=2GuZlr3V4NmdWt8DPnAjUoO38tsH5BPs&limit=10";
-
+        var reaction = $(this).attr("data-name");
+        ifAddMore = reaction;
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + reaction + "&api_key=2GuZlr3V4NmdWt8DPnAjUoO38tsH5BPs&limit=10";
+        offset = 0;
+        max = 10;
+    }
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
 
-        console.log(response);
-
-        for (let i = 0; i < response.data.length; i++) {
+        for (let i = offset; i < max; i++) {
             let newGifDiv = $("<div>");
             newGifDiv.addClass("float-left m-2 border rounded p-2");
 
@@ -46,6 +61,10 @@ function displayGifs() {
             rating.text("Rating: " + response.data[i].rating);
             newGifDiv.append(rating);
 
+            // One-Click Download
+            // works fine in Internet Explorer
+            // But not in Chrome or Firefox (Opens url instead).
+
             var aTag = $("<a>");
             aTag.attr("href", response.data[i].images.original.url);
             aTag.attr("download", "gif");
@@ -58,8 +77,10 @@ function displayGifs() {
             aTag.append(downloadBtn);
             newGifDiv.append(aTag);
 
+            // Add to Favorites space
+
             var fav = $("<button>");
-            fav.addClass("fav glyphicon glyphicon-star-empty btn btn-lg");
+            fav.addClass("fav glyphicon glyphicon-star btn btn-lg");
             fav.attr("data-still", response.data[i].images.original_still.url);
             fav.attr("data-animate", response.data[i].images.original.url);
             fav.attr("data-toggle", "tooltip");
@@ -72,8 +93,24 @@ function displayGifs() {
 
     });
 
+    if (addPresent) {
+        $("#add").remove();
+        addPresent = false;
+    }
+
+    // Add more Button to add more gifs of same kind
+    if (max != 25) {
+        var addMore = $("<button>");
+        addMore.addClass("add-more my-5 btn btn-danger btn-lg");
+        addMore.attr("id", "add");
+        addMore.text("5 more from " + ifAddMore);
+        $("#form").append(addMore);
+        addPresent = true;
+    }
+
 }
 
+// Update Favorites div
 function updateFavorites() {
     $(".favorites").empty();
     for (let i = 0; i < favorites.length; i++) {
@@ -86,20 +123,22 @@ function updateFavorites() {
     }
 }
 
+// To make element to blink
 setInterval(function () {
     $('blink').each(function () {
         $(this).toggle();
     });
 }, 500);
 
+// Main Routine
 $(document).ready(function () {
     createButtons();
 
+    // Add more buttons 
     $("#add-reaction").on("click", function () {
 
         event.preventDefault();
         let newReaction = $("#new-reaction").val().trim().toLowerCase();
-        console.log(newReaction);
         if (newReaction != "") {
             if ((reactions.indexOf(newReaction)) < 0) {
                 reactions.push(newReaction);
@@ -115,6 +154,7 @@ $(document).ready(function () {
 
     $(document).on("click", ".reaction", displayGifs);
 
+    // Toggle animate and still
     $(document).on('click', '.gif', function () {
         let state = $(this).attr("data-state");
         if (state == "still") {
@@ -128,10 +168,26 @@ $(document).ready(function () {
         }
     });
 
+    // Action listener for Add-To-Fav
     $(document).on('click', '.fav', function () {
         if ((favorites.indexOf($(this).attr("data-animate"))) < 0) {
             favorites.push($(this).attr("data-animate"));
+            localStorage.clear();
+            localStorage.setItem("favArray", favorites);
         }
         updateFavorites();
     });
+
+    // Action Listener for Add-More button
+    $(document).on('click', '.add-more', function (event) {
+        event.preventDefault();
+        displayGifs("add");
+    });
 });
+
+// Initialize page with contents from local storage
+if (localStorage.getItem("favArray").length > 0) {
+    var temp = localStorage.getItem("favArray");
+    var favorites = temp.split(",");
+    updateFavorites();
+}
